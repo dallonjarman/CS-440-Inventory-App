b17b877201f74385fd8bf33ea8d4ae59bbf4956e
Dallon Jarman <dmj296@nau.edu>
2025-02-23 22:53:56 -0700

    remvoed the secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from .models import db
 import os
+from dotenv import load_dotenv  # added import
+
+load_dotenv()  # load environment variables
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'secret'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # updated to load from .env
     db.init_app(app)
 
     with app.app_context():
